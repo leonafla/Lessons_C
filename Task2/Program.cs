78b6b72881f3e98ce029c73e137c13774bfcaab2
Lina <leonafla@gmail.com>
2024-02-24 00:17:52 +0200

    lesson1:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿///целочисленные переменные
+int i1 = 5;
+int i2 = 6;
+int sum1 = i1 +i2;
+Console.WriteLine(sum1);
+///дабл и целое
+double d1 = 5.5;
+double sum2 = i1 + d1;
+Console.WriteLine(sum2);
+//лонг и целое
+long l1 = 6;
+long sum3 = i1 + l1;
+Console.WriteLine(sum3);
+///перемножение 
+int product1 = i1 * i2;
+Console.WriteLine(product1);
+double product2 = i1 * d1;
+Console.WriteLine(product2);
+long product3 = i1 * l1;
+Console.WriteLine(product3);
