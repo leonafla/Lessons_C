21ee41004e00d54c3ebb30d3a10808fb767d8135
Lina <leonafla@gmail.com>
2024-02-24 00:00:43 +0200

    add some variable

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+Console.WriteLine(d);
+
+char c = '!';
+Console.WriteLine(c);
+
+bool b = true;
+Console.WriteLine(b);
